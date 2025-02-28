2484428b328b9cf46be1098259506c782acaaa45
MarkPeter19 <99884822+MarkPeter19@users.noreply.github.com>
2025-02-28 22:08:22 +0200

    added back planner

--- MePlusPlusFE/app/(stack)/planner.tsx
+++ MePlusPlusFE/app/(stack)/planner.tsx
@@ -7,8 +7,15 @@ export default function PlannerScreen() {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Planner Page</Text>
-      <Button title="Go Back" onPress={() => router.back()} />
+      <Text style={styles.title}>Select a plan type</Text>
+      <Button 
+        title="Add Code Practice" 
+        onPress={() => router.push("/(stack)/select/code")} 
+      />
+      <Button 
+        title="Select Sport Activity" 
+        onPress={() => router.push("/(stack)/select/sport")} 
+      />
     </View>
   );
 }
@@ -18,6 +25,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+    gap: 20, // Gombok közti távolság
   },
   title: {
     fontSize: 24,
