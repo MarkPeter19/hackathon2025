69ffa29866a4d55d8232a570fed27a22c587a7b3
aksza <suciu.aksza@student.ms.sapientia.ro>
2025-03-01 13:05:01 +0200

    updated db

---
+++ MePlusPlusBE/Migrations/20250301105413_NewThird.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MePlusPlusBE.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewThird : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "IconName",
+                table: "FlipCardCategories",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IconName",
+                table: "FlipCardCategories");
+        }
+    }
+}
--- MePlusPlusBE/Models/FlipCardCategory.cs
+++ MePlusPlusBE/Models/FlipCardCategory.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Type { get; set; }
+        public string IconName { get; set; }
         public ICollection<FlipCard>? FlipCards { get; set; }
     }
 }
