1ee4e20451af482c7d1313cef17a4eec964cc26c
aksza <suciu.aksza@student.ms.sapientia.ro>
2025-03-02 10:32:54 +0200

    added ai generated quests

---
+++ MePlusPlusBE/Dto/NewCheckQuest.cs
@@ -0,0 +1,10 @@
+﻿namespace MePlusPlusBE.Dto
+{
+    public class NewCheckQuest
+    {
+        public string CategoryName { get; set; }
+        public string RecommendedActivity { get; set; }
+        public string Measure { get; set; }
+        public int XpLevel { get; set; }
+    }
+}
---
+++ MePlusPlusBE/Dto/NewQuest.cs
@@ -0,0 +1,8 @@
+﻿namespace MePlusPlusBE.Dto
+{
+    public class NewQuest
+    {
+        public string SelectedCategoryFlip { get; set; }
+        public List<NewCheckQuest> CheckQuests { get; set; }
+    }
+}
